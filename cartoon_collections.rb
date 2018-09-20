0d884fefafe65e4c8941608985447f176de7414a
Keren-Happuc Fleurimond <keren@noukod.com>
2018-09-20 18:10:47 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,5 +20,5 @@ end
 def find_the_cheese(snacks)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-snacks.find { |i| cheese_types.include?(i) }
+snacks.find { |s| cheese_types.include?(s) }
 end
