6470b3602868510f7111c6baf85221aa29ce2f76
Keren-Happuc Fleurimond <keren@noukod.com>
2018-09-20 18:05:10 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,10 +14,10 @@ end
 
 def long_planeteer_calls(short_words, assorted_words)# code an argument here
   # Your code here
-  
+
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(snacks, soup)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
