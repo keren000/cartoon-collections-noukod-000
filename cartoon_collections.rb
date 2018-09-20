2c6b77907c64659d624f6592d87c838bbba4d552
Keren-Happuc Fleurimond <keren@noukod.com>
2018-09-20 18:11:10 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,7 +8,6 @@ end
 
 def summon_captain_planet(planeteer_calls)# code an argument here
   # Your code here
-  #planeteer_calls.collect { |x| "#{x.capitalize}!" }
     planeteer_calls.map! {|x| x.capitalize }
 end
 
