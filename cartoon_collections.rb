cf2d40e8f4dad26cf8aff5f1f9c4aef4983042ed
Keren-Happuc Fleurimond <keren@noukod.com>
2018-09-20 18:28:31 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,23 +1,28 @@
-def roll_call_dwarves(arr)# code an argument here
-  # Your code here
-  arr.each_with_index do |arr1, arr2|
-    arr2 += 1
-    puts "#{arr2}. #{arr1}"
-  end
+def roll_call_dwarves(dwarves)
+
+  dwarves.each_with_index { |dwarf, index| puts "#{index + 1} #{dwarf}"}
+
 end
 
-def summon_captain_planet(planeteer_calls)# code an argument here
-  # Your code here
-    planeteer_calls.map! {|x| x.capitalize }
+
+
+def summon_captain_planet(planeteer_calls)
+
+  planeteer_calls.collect { |i| "#{i.capitalize}!" }
+
 end
 
-def long_planeteer_calls(lpb)# code an argument here
-  # Your code here
- lpb.any? { |p| p.length > 4 }
+
+def long_planeteer_calls(long_planeteer_calls)
+
+  long_planeteer_calls.any? { |i| i.length > 4 }
+
 end
 
-def find_the_cheese(snacks)# code an argument here
-  # the array below is here to help
+def find_the_cheese(cheese)
+
   cheese_types = ["cheddar", "gouda", "camembert"]
-snacks.find { |s| cheese_types.include?(s) }
+
+  cheese.find { |i| cheese_types.include?(i) }
+
 end
