86873c2f404f6974266f6df9685c786b1ea91e21
Keren-Happuc Fleurimond <keren@noukod.com>
2018-09-20 18:28:47 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,7 @@
 def roll_call_dwarves(dwarves)
-
   dwarves.each_with_index { |dwarf, index| puts "#{index + 1} #{dwarf}"}
 
 end
-
 
 
 def summon_captain_planet(planeteer_calls)
@@ -11,7 +9,6 @@ def summon_captain_planet(planeteer_calls)
   planeteer_calls.collect { |i| "#{i.capitalize}!" }
 
 end
-
 
 def long_planeteer_calls(long_planeteer_calls)
 
